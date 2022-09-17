7c45eba90869109a77a5610c0ab755d3cecc399f
KedarGhadge <kghadge84@gmail.com>
2022-09-17 16:57:07 +0530

    second commit

--- ApiClient/src/components/login/login.js
+++ ApiClient/src/components/login/login.js
@@ -1,40 +1,32 @@
-import ko from 'knockout';
-import loginTemplate from 'text!./login.html';
+import ko from "knockout";
+import loginTemplate from "text!./login.html";
 
 class MyLogin {
-   
-    constructor(route) {
-        var self = this;
-        this.message = ko.observable('Welcome to MySite!');
+  constructor(route) {
+    var self = this;
+    this.message = ko.observable("Welcome to MySite!");
 
-        self.doChangeTxt = function() {
-            $.ajax({
-                type: "POST",
-                url: "./home/login",
-                //data: ko.toJSON(new LoginEn(this.userName(), this.password(), this.selectedCustomer().CustomerID)),
-                
-                contentType: 'application/json; charset=utf-8',
-                dataType: "json",
-                success: function (data) {
-                    if(data)
-                    {
-                        //cu.session.customer(self.selectedCustomer());
-                        $("#toptitle").text(data);
-                        
-                    }
-                    else
-                    $("#toptitle").text("No Data");
-                },
-                error: function () {
-                    console.log("login Failed!!");
-                    
-                }
-            });
-            // $("#toptitle").text("New word");
-        }
-    }
-    
-    
+    self.doChangeTxt = function () {
+      $.ajax({
+        type: "POST",
+        url: "./home/login",
+        //data: ko.toJSON(new LoginEn(this.userName(), this.password(), this.selectedCustomer().CustomerID)),
+
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+          if (data) {
+            //cu.session.customer(self.selectedCustomer());
+            $("#toptitle").text(data);
+          } else $("#toptitle").text("No Data");
+        },
+        error: function () {
+          console.log("login Failed!!");
+        },
+      });
+      // $("#toptitle").text("New word");
+    };
+  }
 }
 
 export default { viewModel: MyLogin, template: loginTemplate };
